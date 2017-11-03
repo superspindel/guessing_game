9114677740ea66fb8a0fc2963a722b1fb6110b43
superspindel <viktor.fallman@hotmail.com>
2017-11-03 20:00:43 +0100

    updated with proper implementation of assignment

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use std::cmp::Ordering;
 
 
 fn main() {
-    let mut guess_vector = Vec::new();
+    let mut guess_vector: Vec<(u32, String)> = Vec::new();
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("{}", secret_number);
     let mut number_of_tries = 0;
     loop {
         println!("Please input your guess");
-        let guess: u32 = match get_user_input()
+        number_of_tries += 1;
+        let guess: u32 = match get_user_input(&mut guess_vector, number_of_tries)
         {
             Ok(number) => number,
             Err(error_info) => 
@@ -22,18 +22,16 @@ fn main() {
                 continue;
             }
         };
-        number_of_tries += 1;
         println!("You guessed: {}", guess);
         print!("Number of tries: ");    
         println!("{}", number_of_tries);
-        guess_vector.push((number_of_tries, guess));
         match guess.cmp(&secret_number)
         {
             Ordering::Less      => println!("Too small!"),
             Ordering::Greater   => println!("Too big!"),
             Ordering::Equal     => {
                 println!("You win");
-                for &(guess_number, guess_value) in guess_vector.iter().rev().take(3)
+                for &(ref guess_number, ref guess_value) in guess_vector.iter().rev().take(3)
                 {
                     println!("Numbers of guesses: {}, Guessed value: {}", guess_number, guess_value);
                 };
@@ -44,11 +42,12 @@ fn main() {
     }
 }
 
-fn get_user_input() -> Result<u32, String> {
+fn get_user_input(guess_vector: &mut Vec<(u32, String)>, number_of_tries: u32) -> Result<u32, String> {
     let mut guess = String::new();
     match io::stdin().read_line(&mut guess)
     {
         Ok(_) => {
+            guess_vector.push((number_of_tries, guess.clone()));
             match guess.trim().parse()
             {
                 Ok(num) => Ok(num),
