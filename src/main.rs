efbe86441096cb93b96cda80e24a2c8622f06192
superspindel <viktor.fallman@hotmail.com>
2017-11-03 16:55:12 +0100

    Store guesses and number of guesses in hashmap and iterate the hashmap when winning

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@ extern crate rand;
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
 
 fn main() {
-    let mut guess_vector = Vec::new();
+    let mut guess_hashmap: HashMap<u32, String> = HashMap::new();
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1,101);
     println!("{}", secret_number);
     let mut number_of_tries = 0;
     loop {
         println!("Please input your guess");
-        let guess: u32 = match get_user_input()
+        number_of_tries += 1;
+        let guess: u32 = match get_user_input(&mut guess_hashmap, number_of_tries)
         {
             Ok(number) => number,
             Err(error_info) => 
@@ -22,18 +24,16 @@ fn main() {
                 continue;
             }
         };
-        number_of_tries += 1;
         println!("You guessed: {}", guess);
         print!("Number of tries: ");    
         println!("{}", number_of_tries);
-        guess_vector.push((number_of_tries, guess));
         match guess.cmp(&secret_number)
         {
             Ordering::Less      => println!("Too small!"),
             Ordering::Greater   => println!("Too big!"),
             Ordering::Equal     => {
                 println!("You win");
-                for &(guess_number, guess_value) in guess_vector.iter().rev().take(3)
+                for (guess_number, guess_value) in guess_hashmap.iter()
                 {
                     println!("Numbers of guesses: {}, Guessed value: {}", guess_number, guess_value);
                 };
@@ -44,11 +44,12 @@ fn main() {
     }
 }
 
-fn get_user_input() -> Result<u32, String> {
+fn get_user_input(guess_hashmap: &mut HashMap<u32, String>, number_of_tries: u32) -> Result<u32, String> {
     let mut guess = String::new();
     match io::stdin().read_line(&mut guess)
     {
         Ok(_) => {
+            guess_hashmap.insert(number_of_tries, guess.clone());
             match guess.trim().parse()
             {
                 Ok(num) => Ok(num),
